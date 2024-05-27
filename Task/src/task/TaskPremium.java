33e7024e38f01d90714d4a16675c92d21492d321
Nicholasxav <155067595+Nicholasxav@users.noreply.github.com>
2024-05-27 19:04:34 +0700

    Update TaskPremium.java

--- Task/src/task/TaskPremium.java
+++ Task/src/task/TaskPremium.java
@@ -10,5 +10,17 @@ package task;
  * @author Asus
  */
 public class TaskPremium {
-    
+    private String prioritas;
+
+    public TaskPremium(String judul, String username, String deskripsi, Date TanggalMulai, Date tanggalSelesai, String jenisTugas, taskState taskState) {
+        super(judul, username, deskripsi, TanggalMulai, tanggalSelesai, jenisTugas, taskState);
+    }
+
+    public String getPrioritas() {
+        return prioritas;
+    }
+
+    public void setPrioritas(String prioritas) {
+        this.prioritas = prioritas;
+    }
 }
