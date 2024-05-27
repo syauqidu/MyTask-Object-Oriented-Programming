ebef459cceacb481f1b5888949586d2abbcf704b
Nicholasxav <155067595+Nicholasxav@users.noreply.github.com>
2024-05-27 19:26:21 +0700

    Update TaskPremium.java

--- Task/src/task/TaskPremium.java
+++ Task/src/task/TaskPremium.java
@@ -9,7 +9,7 @@ package task;
  *
  * @author Asus
  */
-public class TaskPremium {
+public class TaskPremium extends Task {
     private String prioritas;
 
     public TaskPremium(String judul, String username, String deskripsi, Date TanggalMulai, Date tanggalSelesai, String jenisTugas, taskState taskState) {
