a4c622228df5726abf8ff18213b30e6c946ee20c
Nicholasxav <155067595+Nicholasxav@users.noreply.github.com>
2024-05-27 18:11:25 +0700

    Add files via upload

---
+++ Task/src/task/Task.java
@@ -0,0 +1,116 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package task;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.EnumSet;
+import java.util.List;
+/**
+ *
+ * @author Asus
+ */
+public class Task implements ShowData {
+    private String judul;
+    private String username;
+    private String deskripsi;
+    private Date TanggalMulai;
+    private Date tanggalSelesai;
+    private String jenisTugas;
+    protected String taskState;
+
+    @Override
+    public void showInfo() {
+        System.out.println("Judul: " + this.judul);
+        System.out.println("Username: " + this.username);
+        System.out.println("Deskripsi: " + this.deskripsi);
+        System.out.println("Tanggal Mulai: " + this.TanggalMulai);
+        System.out.println("Tanggal Selesai: " + this.tanggalSelesai);
+        System.out.println("Jenis Tugas: " + this.jenisTugas);
+        System.out.println("Task State: " + this.taskState);
+    }
+
+    @Override
+    public void sorting() {
+        List<Task> tasks = new ArrayList<>(); 
+        Collections.sort(tasks, (t1, t2) -> t1.judul.compareTo(t2.judul));
+        for (Task task : tasks) {
+            task.showInfo();
+        }
+    }
+    private enum taskState{
+        NOT_STARTED,
+        IN_PROGRESS,
+        COMPLETED,
+        ON_HOLD
+    }
+
+    public String getTaskState() {
+        return taskState;
+    }
+
+    public void setTaskState(String taskState) {
+        this.taskState = taskState;
+    }
+    
+    public Task(String judul, String username, String deskripsi, Date TanggalMulai, Date tanggalSelesai, String jenisTugas, taskState taskState){
+        this.judul = judul;
+        this.username = username;
+        this.deskripsi = deskripsi;
+        this.TanggalMulai = TanggalMulai;
+        this.tanggalSelesai = tanggalSelesai;
+        this.jenisTugas = jenisTugas;
+    }
+
+    public String getJudul() {
+        return judul;
+    }
+
+    public void setJudul(String judul) {
+        this.judul = judul;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getDeskripsi() {
+        return deskripsi;
+    }
+
+    public void setDeskripsi(String deskripsi) {
+        this.deskripsi = deskripsi;
+    }
+
+    public Date getTanggalMulai() {
+        return TanggalMulai;
+    }
+
+    public void setTanggalMulai(Date TanggalMulai) {
+        this.TanggalMulai = TanggalMulai;
+    }
+
+    public Date getTanggalSelesai() {
+        return tanggalSelesai;
+    }
+
+    public void setTanggalSelesai(Date tanggalSelesai) {
+        this.tanggalSelesai = tanggalSelesai;
+    }
+
+    public String getJenisTugas() {
+        return jenisTugas;
+    }
+
+    public void setJenisTugas(String jenisTugas) {
+        this.jenisTugas = jenisTugas;
+    }
+    
+}
---
+++ Task/src/task/TaskReguler.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package task;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Asus
+ */
+public class TaskReguler extends Task {
+    private String prioritas;
+    private int limitTask;
+
+    public TaskReguler(String prioritas, int limitTask, String judul, String username, String deskripsi, Date TanggalMulai, Date tanggalSelesai, String jenisTugas, TaskState askState) {
+        super(judul, username, deskripsi, TanggalMulai, tanggalSelesai, jenisTugas, askState);
+        this.prioritas = prioritas;
+        this.limitTask = limitTask;
+    }
+
+    public String getPrioritas() {
+        return prioritas;
+    }
+
+    public void setPrioritas(String prioritas) {
+        this.prioritas = prioritas;
+    }
+
+    public int getLimitTask() {
+        return limitTask;
+    }
+
+    public void setLimitTask(int limitTask) {
+        this.limitTask = limitTask;
+    }
+    
+}
